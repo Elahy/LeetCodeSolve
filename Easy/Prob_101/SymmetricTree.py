a942ee6350cf976cd09c0af579cfeccb158f3812
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2021-10-20 22:38:29 +0600

    Solved Symmetric Tree

---
+++ Easy/Prob_101/SymmetricTree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root == None:
+            return True
+        else:
+            return self.isMirror(root.left, root.right)
+        
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        
+        if left.val == right.val:
+            outerPair = self.isMirror(left.left, right.right)
+            innerPair = self.isMirror(left.right, right.left)
+            return outerPair and innerPair
+        else:
+            return False
