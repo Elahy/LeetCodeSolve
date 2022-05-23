4b580aaa85290dc6d4d9e6289fa32d2e9f8ca8d3
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2022-05-23 11:06:36 +0600

    richest wealth

---
+++ Easy/1500+/Prob_1672/RichestCustomerWealth.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        richest = 0
+        for wealth in accounts:
+            richest = max(richest, sum(wealth))
+        return richest
+        
